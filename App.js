cb382cc4c3afb2ea3395c5df17b97cf07d9d8163
vmydillic <vladyslav@idyllicvision.com>
2021-10-13 17:09:08 +0300

    cross messaging

--- App.js
+++ App.js
@@ -7,44 +7,11 @@
  */
 
 import React, { useRef, useState } from 'react';
-import { LogBox, TextInput } from 'react-native';
-// import * as s from 'react-native-snarkjs';
-// var RNFS = require('react-native-fs');
 import { WebView } from 'react-native-webview';
-
 import { useBridge } from "react-native-react-bridge";
-import webApp from "./components/web";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  Field,
-  Input,
-  useColorScheme,
-  View,
-  Button,
-  TextView,
-} from 'react-native';
 
-import {
-  Colors,
-  Header,
-} from 'react-native/Libraries/NewAppScreen';
-
-
-const inputs = {"a":3, b:"4"}
-
-let wasmFile = "https://5d3f-195-39-242-119.ngrok.io/circuit.wasm";
-let zkeyFile = "https://5d3f-195-39-242-119.ngrok.io/circuit_final.zkey";
-let verificationKey = "https://5d3f-195-39-242-119.ngrok.io/verification_key.json";
-
-// import   * as s  from "react-native-snarkjs/build/main";
 const App = () => {
 
-  const myWebView = useRef();
-  // console.log(s);
   const [a, setA] = useState("3");
 	const [b, setB] = useState("11");
 
@@ -62,7 +29,6 @@ const App = () => {
 		setB(e.target.value);
 	};
 
-  // console.log(webApp)
 
   const { ref, onMessage, emit } = useBridge((message) => {
     // emit sends message to React
@@ -79,87 +45,24 @@ const App = () => {
     }
   });
 
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const runProofs = async () => {
-    console.log(">>>>>>>>>>>>>>>>>>> runProofs")
-
-    s.groth16.fullProve(inputs, wasmFile, zkeyFile,console).then(({ proof, publicSignals }) =>{
-      console.log(proof)
-      console.log(publicSignals)
-
-      fetch(verificationKey).then(function (res) {
-         res.json().then((res2)=>{
-          console.log("start verifikation",res2)
-          setText(publicSignals.toString())
-
-          s.groth16.verify(res2, publicSignals, proof,console).then((res3)=>{
-            console.log(res3)
-            setText(res3.toString())
-
-          })
-        })
-
-      });
-
-
-    });
-
-
-  }
-// function  onMessage( event ) {
-//     console.log( "On Message", event.nativeEvent.data );
-// }
-//  function  sendPostMessage () {
-//     console.log( "Sending post message" );
-//     console.log("here ", myWebView.current)
-//     myWebView.current.postMessage( "Post message from react native")
-//     // myWebView.current.postMessage( "Post message from react native" );
-// }
-// console.log(source)
 
   const onMymessage = (e) => {
     const { nativeEvent } = e;
     const { data } = nativeEvent;
 
-    // console.log(nativeEvent);
+    console.log("message recevied", e)
     console.log(JSON.parse(data));
-    // emit({ type: "loader", data: "succeeded!" });
-    emit({ type: "towebview", data: "succeeded!" });
+    emit({ type: "towebview", data: "this can be input data for proof!" });
   }
 return (
   <WebView
     // ref, source and onMessage must be passed to react-native-webview
     ref={ref}
-    source={{ uri: 'https://e306-95-158-35-18.ngrok.io' }}
+    source={{ uri: 'https://461c-89-22-47-19.eu.ngrok.io' }}
     style={{marginTop:0, height:'100%' , backgroundColor: 'green'}}
-    // onMessage={onMessage}
     onMessage={onMymessage}
   />
 );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
